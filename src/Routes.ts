2e2b18fbdc1f3828f0c4531e1bd58e18cb67e30f
sourabhdadapure <sourabhd@Sourabhs-Air.home>
2020-03-15 18:06:11 -0400

    Adds navigation stack package

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,3 +1,9 @@
-import * as React from 'react'
-import {View} from 'react-native'
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
