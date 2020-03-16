d8541c874f8e47d05fa5ca553eb7a8d925960780
sourabhdadapure <sourabhdadapure@gmail.com>
2020-03-16 00:53:39 -0400

    Updates routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,21 +3,27 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from './views/HomeScreen'
+import SplashScreen from './views/SplashScreen'
+import IntroScreen from './views/IntroScreen'
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator headerMode="none">
+      <Stack.Screen  name="Intro" component={IntroScreen} />
+      <Stack.Screen  name="Splash" component={SplashScreen} />
+        <Stack.Screen  name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
