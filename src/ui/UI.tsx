bc61e33706861b815df87ef869569df5d551564f
sourabhdadapure <sourabhdadapure@gmail.com>
2020-03-17 19:30:29 -0400

    Adds UI.tsx

---
+++ src/ui/UI.tsx
@@ -0,0 +1,12 @@
+import { Buttons } from "./buttons/Buttons";
+import { Labels } from "./labels/Labels";
+import { Colors } from "./Colors";
+
+
+const UI = {
+    Buttons: Buttons,
+    Labels: Labels,
+    Colors:  Colors,
+}
+
+export default UI;
