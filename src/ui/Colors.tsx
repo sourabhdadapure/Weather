bd2f230c880ff903cc1c6ecef5473adcd8c30c2b
sourabhdadapure <sourabhdadapure@gmail.com>
2020-03-17 15:11:59 -0400

    Adds colors

---
+++ src/ui/Colors.tsx
@@ -0,0 +1,33 @@
+export const Colors = {
+    Background: {
+        Picker: "#141414",
+        Delete: "red",
+        Card: "#19191c"
+    },
+    Buttons: {
+        White: "#FFFFFF",
+        Blue: "#5289fe",
+        CTA: "#474951",
+        DarkGray: "#19191C",
+        Gray: "#828994",
+        Active: "#5289fe"
+    },
+    Icons: {
+        White: "white",
+        Gray: "#7a7c86",
+        Black: "black",
+        Blue: "#5289fe"
+    },
+    Temperature: {
+        Warm: "#EA3760",
+        Cool: "#1862FF",
+        Neutral: "white",
+        Disabled: "#9FA4BE"
+    },
+    Labels: {
+        White: "white",
+        Gray: "#98A0B4",
+        DarkGray: "#7A7C86",
+        Link: "#5289fe"
+    }
+};
