15a9dbed2da45e9ed2e9403842c50a5b74a4d416
sourabhdadapure <sourabhdadapure@gmail.com>
2020-03-17 13:54:43 -0400

    removes UI folder

--- src/views/IntroScreen.tsx
+++ src/views/IntroScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import {View, Text} from 'react-native'
-import Buttons from '../UI/Buttons'
 
 
 export default class IntroScreen extends React.Component{
@@ -8,7 +7,6 @@ export default class IntroScreen extends React.Component{
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
               <Text>Welcome</Text>
-              <Buttons buttonType="link" title="Test" onPress={()=>{}}/>
             </View>
           );
     }
